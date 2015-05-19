437cc32ac919e2f86e59fabfe75532924e933e0c
Nukr <nukrs.w@gmail.com>
2015-05-20 00:36:12 +0800

    fix

--- Vagrantfile
+++ Vagrantfile
@@ -13,7 +13,8 @@ Vagrant.configure(2) do |config|
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "ubuntu/trusty64"
-  config.vm.provision :shell, path: "bootstrap.sh"
+  config.vm.provision :shell, path: "bootstrap.sh", privileged: false
+  config.vm.network :forwarded_port, host: 9999, guest: 80
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-apt-get update
-apt-get install -y git
-locale-gen zh_TW.UTF-8
+sudo apt-get update
+sudo apt-get install -y git-core curl
+sudo locale-gen zh_TW.UTF-8
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.1/install.sh | bash
-. /home/vagrant/.bashrc
+curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
+
+echo "source /home/vagrant/.nvm/nvm.sh" >> /home/vagrant/.profile
+source /home/vagrant/.profile
 
 nvm install iojs
+nvm alias default iojs
